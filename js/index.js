ba8c684ed96d0ad03d97998ce18f3b5fe0f2bcdb
gitkevinvera <ceo@webrend.com>
2024-09-13 07:36:07 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+import { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://maclellan-family-website.s3.us-east-2.amazonaws.com/firebase-init.js';
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, continue to show the restricted page.
+    } else {
+      // No user is signed in, redirect to login page.
+      window.location.href = 'https://webrendhq.github.io/Maclellan-Frontend/home.html';
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('enter-site-button');
     
@@ -5,3 +16,4 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'https://webrendhq.github.io/Maclellan-Frontend/sign-in.html';
     });
 });
+
