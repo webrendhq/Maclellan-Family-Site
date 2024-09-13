d33b495f92dff4b7803446408c006ecf1b709c59
gitkevinvera <ceo@webrend.com>
2024-09-13 08:11:08 -0400

    Update sign-out.js

--- js/sign-out.js
+++ js/sign-out.js
@@ -1,4 +1,6 @@
-import { auth, signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { auth } from 'https://maclellan-family-website.s3.us-east-2.amazonaws.com/firebase-init.js';
+
 // Your Firebase config
 const firebaseConfig = {
     apiKey: "AIzaSyCqGV5J3if7mJoH464xGx6bZ5wgU_wMn3I",
