9ca960f0d70231a4cf0147ee7addf3c5b7e56665
gitkevinvera <ceo@webrend.com>
2024-09-13 02:37:35 -0400

    sign in css

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('enter-site-button');
+    
+    button.addEventListener('click', function() {
+        window.location.href = 'https://maclellen.com/sign-in.html';
+    });
+});
