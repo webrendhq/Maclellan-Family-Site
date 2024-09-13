9ece61fdd204a005654f115ae58b7738e7bfe6b6
gitkevinvera <ceo@webrend.com>
2024-09-13 05:40:01 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('enter-site-button');
     
     button.addEventListener('click', function() {
-        window.location.href = 'https://maclellen.com/sign-in.html';
+        window.location.href = '/sign-in.html';
     });
 });
