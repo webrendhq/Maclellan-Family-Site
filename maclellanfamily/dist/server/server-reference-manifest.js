f1990d9298006deb05eb3163c3c8f50f0a89db82
gitkevinvera <ceo@webrend.com>
2024-11-28 08:11:16 -0500

    new builds

--- maclellanfamily/dist/server/server-reference-manifest.js
+++ maclellanfamily/dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cylgEv0lLRimp53abk439SWZWiA2R4GKTQdlcwCAepY=\"\n}"
